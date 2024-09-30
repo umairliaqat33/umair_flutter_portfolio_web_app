a2dfff608c5ddc50f62321e22970e8ef24949917
Umair <umairliaqat552@gmail.com>
2024-09-30 13:33:47 +0500

    removed google maps and other api keys

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,19 +1,7 @@
 String phonePattern =
     r'^((\+44\s?\d{4}|\(?\d{5}\)?)\s?\d{6})|((\+44\s?|0)7\d{3}\s?\d{6})$';
 String emailPatter = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]";
-String googleMapsApiKey = "AIzaSyAGVc_fxVnley2T6yDwwzkuZ7nY95Zw1CE";
 String currency = "£";
 String incrementSymbol = "+";
 String decrementSymbol = "-";
 String phoneNumberPattern = '^(?:[+0]9)?[0-9]{11}\$';
-String contactEmail = "info@avgigs.com";
-String mapsAPIKey = 'AIzaSyCaCSJ0BZItSyXqBv8vpD1N4WBffJeKhLQ';
-String cloudMessagingApi =
-    "AAAA1uwK3Lw:APA91bGHJ-CSKAwSfn6-rTxegFhLw2BuZvo6_vsPxMN5b20oUd15B8HgUliOE--8sX5WG3AFeUnY-_-ZX_lUM5N3VZQH8tFmlGM4-zjZMvB9TEUcz9e_sZJ9V75kl9-1_7TIGbVqoTyU";
-
-// SizeConfig.width20(context) * 5.6 small without icon (2 words)
-// SizeConfig.width20(context) * 7 small with icon (2 words)
-// SizeConfig.width20(context) * 7 medium without icon (3 words)
-// SizeConfig.width20(context) * 8 medium with icon (3 words)
-// SizeConfig.width20(context) * 12 big without icon (5 words)
-// SizeConfig.width20(context) * 14 big with icon (5 words)
