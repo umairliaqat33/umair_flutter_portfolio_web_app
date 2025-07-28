f9d7655ef86abfad6888330dee14b2cb0a1db8c0
Umair <umairliaqat552@gmail.com>
2025-07-28 21:41:54 +0500

    updated cv link

--- lib/utils/app_strings.dart
+++ lib/utils/app_strings.dart
@@ -1,8 +1,8 @@
 class AppConstants {
   static const String resume =
-      'https://drive.google.com/file/d/1ON4gsfxDJSGv6qJzI0CkDB65muAu5dik/view?usp=sharing';
+      'https://drive.google.com/file/d/1ON4gsfxDJSGv6qJzI0CkDB65muAu5dik/view?usp=drive_link';
   static const String cv =
-      'https://drive.google.com/file/d/1hsqVAIV0WZTa5ZO5IWjfgX8qIF4yYycD/view?usp=sharing';
+      'https://drive.google.com/file/d/1ON4gsfxDJSGv6qJzI0CkDB65muAu5dik/view?usp=drive_link';
   static const String whatsapp =
       'https://api.whatsapp.com/send?phone=923134146206';
   static const String linkedIn =
